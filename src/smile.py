b428dfac3d931ef19d1ef01c2b6dc97e7e7dc44e
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2017-02-25 18:19:04 +0530

    dummy loadable extension complete

--- src/smile.py
+++ src/smile.py
@@ -3,19 +3,19 @@
 
 import inkex
 
-class MyExtensionName(inkex.Effect):
+class Smile(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
-        self.OptionParser.add_option('-x', '--something', action='store', type='string', dest='optionName',
-                                     default='defaultvalue', help='Helper text for this option')
+        self.OptionParser.add_option('-s', '--some_name', action='store', type='string', dest='optionName', default='defaultvalue', help='Helper text for this option')
 
     def effect(self):
         option = self.options.optionName
         self._main_function(option)
 
     def _main_function(self, option):
-        print 'Hello world'
+        svg = self.document.getroot()
+        layer = inkex.etree.SubElement(svg, 'g')
 
 if __name__ == '__main__':
-    MyExtension = MyExtensionName()
+    MyExtension = Smile()
     MyExtension.affect()
